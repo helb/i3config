ef5cf83da61d6adc28fbe5e8fbc7f2dc3ee4d7b3
helb <helb@helb.cz>
2016-03-03 15:55:38 +0100

    use universal autorun script, update config

--- autostart.sh
+++ autostart.sh
@@ -1,27 +1,35 @@
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5
+#!/bin/zsh
 
-echo 220 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
+HOSTNAME=$(/bin/hostname)
 
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 1 1 0
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edges" 0 0 0 4582
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Scrolling Distance" 300 300
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Scrolling" 1 1
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Jumpy Cursor Threshold" 250
-xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Palm Detection" 1
+case $HOSTNAME in
+kookaburra)
+    xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
+    xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
+    xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
+    xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5
+    echo 220 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 1 1 0
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edges" 0 0 0 4582
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Scrolling Distance" 300 300
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Scrolling" 1 1
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Jumpy Cursor Threshold" 250
+    xinput --set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Palm Detection" 1
+    xrandr --dpi 131 &
+    synclient TouchpadOff=1 &
+    ;;
+magpie)
+    xrandr --dpi 96 &
+    ;;
+raven)
+    xrandr --dpi 94 &
+    ;;
+esac
 
-
-urxvtd -q -f -o && urxvtc -pe tabbedex &
 hsetroot -full ~/.local/wallpapers/wall.jpg &
-#synclient TouchpadOff=1 &
-#compton -C -b -c -e 0.75 &
 xbindkeys &
 setxkbmap vok &
 devmon -g -s --exec-on-drive "notify-send mounted %l" &
-#tint3 -c ~/.config/panel.tint3rc &
 nm-applet &
-#cbatticon -l 10 -r 5 -u 15 -x "/home/helb/bin/restartpanel.sh" &
 xautolock -time 5 -locker slock &
-
+urxvtd -q -f -o && urxvtc -pe tabbedex &
